fc9c40f6b216f4099c6d51825f5258cb8a3ff667
Meenakshi Pathania <meenakship@ocodewire.com>
2022-05-24 18:53:16 +0530

    commit with code

--- reactapp/src/components/Service/index.js
+++ reactapp/src/components/Service/index.js
@@ -25,7 +25,7 @@ function Service() {
             <section className="blogpage-section">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-12 col-md-7">
+                        <div className="col-lg-12 col-md-12">
                             <ServicesApp />
                         </div>
                         {/* <div className="col-lg-4 col-md-5">
